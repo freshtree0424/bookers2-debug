87c450cda974ef5c0a734c2a0546ab483d7e86a1
EC2 Default User <ec2-user@ip-172-31-21-1.ap-northeast-1.compute.internal>
2024-02-23 03:59:10 +0000

    [Add]追加編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,22 @@
 class BooksController < ApplicationController
-
+ before_action :is_matching_login_user, only: [:edit, :update]
+ 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
-    @book = Book.all
-    @books = Book.new
+    @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
+      @user = current_user
       @books = Book.all
       render :index
     end
@@ -33,9 +35,9 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    book.destoy
     redirect_to books_path
   end
 
